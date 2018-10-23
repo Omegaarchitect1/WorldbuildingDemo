ad09e0a71c4a05c067280aa27ece9c833038560d
Mitchell Quigg <Mitchell.Quigg@iam.local>
2018-10-23 13:37:00 -0500

    Added movement

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    [SerializeField]
+    private float MoveSpeed = 0;
+
+    [SerializeField]
+    private float JumpForce = 0;
+
+ 
+    private Rigidbody2D rb2d;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        PlayerControl();
+        //Jump();
+    }
+
+	private void PlayerControl()
+    {
+        float run = Input.GetAxis("Horizontal") * MoveSpeed;
+        Vector3 movement = new Vector3(run, 0, 0);
+
+        rb2d.AddForce(Vector2.right * movement);
+       
+    }
+
+  
+
+  // private void Jump()
+   //{
+    // if(Input.GetButton("Jump"))
+     //   {
+         
+      //  }
+     
+  // }
+			
+}
